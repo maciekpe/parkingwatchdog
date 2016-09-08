d3f5852177cc737d131fa7be8d5a4d5759a19ba4
Maciej Peruga <maciej.peruga@4finance.com>
2016-09-09 01:16:04 +0200

    working version

--- parkingwatchdog/parkingwatchdog/ViewController.m
+++ parkingwatchdog/parkingwatchdog/ViewController.m
@@ -17,16 +17,19 @@
 @interface ViewController ()<CLLocationManagerDelegate>
 
 @property (nonatomic, strong) GTLService *service;
-@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UITextView *textViewRight;
 @property (strong, nonatomic) IBOutlet UIView *mainView;
 @property (weak, nonatomic) IBOutlet UIButton *workBtn;
 @property (weak, nonatomic) IBOutlet UIButton *homeBtn;
 @property (weak, nonatomic) IBOutlet UIButton *analysisBtn;
+@property (weak, nonatomic) IBOutlet UITextView *textViewLeft;
 
 @end
 
 static NSString *const kKeychainItemName = @"parkingwatchdog";
 static NSString *const kClientID = @"829224129199-o2i8n6lijmj02sqftomb2g0h5gtan4en.apps.googleusercontent.com";
+static NSString *const kWorkURL = @"comgooglemaps://?saddr=Faraona+11,+Pruszkow&daddr=Aleje+Jerozolimskie+92,+Warszawa&views=traffic&directionsmode=driving";
+static NSString *const kHomeURL = @"comgooglemaps://?daddr=Faraona+11,+Pruszkow&saddr=Aleje+Jerozolimskie+92,+Warszawa&views=traffic&directionsmode=driving";
 
 @implementation ViewController
 
@@ -41,17 +44,12 @@ static NSString *const kClientID = @"829224129199-o2i8n6lijmj02sqftomb2g0h5gtan4
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.service = [[GTLService alloc] init];
-    self.service.authorizer =
-    [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:kKeychainItemName
-                                                          clientID:kClientID
-                                                      clientSecret:nil];
-    //NSLog(@"Start");
-    //[self listMajors];
-    _locationMgr = [self createLocationManager];
-    self.locationMgr.delegate = self;
-    //[self.locationMgr startUpdatingLocation];
-    
+    [self initGoogleServices];
+    [self initViewElements];
+    NSLog(@"END");
+}
+
+- (void)initViewElements {;
     if(_workBtn != nil){
         _workBtn.layer.cornerRadius = 5;
     }
@@ -64,15 +62,36 @@ static NSString *const kClientID = @"829224129199-o2i8n6lijmj02sqftomb2g0h5gtan4
         _analysisBtn.layer.cornerRadius = 5;
     }
     
-    NSLog(@"END");
-    // Do any additional setup after loading the view, typically from a nib.
+    if(_textViewRight != nil){
+        _textViewRight.layer.cornerRadius=5;
+    }
+    
+    if(_textViewLeft != nil){
+        _textViewLeft.layer.cornerRadius=5;
+    }
+    
+
+}
+
+- (void)initGoogleServices {;
+    self.service = [[GTLService alloc] init];
+    self.service.authorizer =
+    [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:kKeychainItemName
+                                                          clientID:kClientID
+                                                      clientSecret:nil];
+}
+
+- (void)initLocationServices {;
+    _locationMgr = [self createLocationManager];
+    self.locationMgr.delegate = self;
+    [self.locationMgr startUpdatingLocation];
 }
 
 - (CLLocationManager* ) createLocationManager {
     if ([CLLocationManager locationServicesEnabled]){
      NSLog(@"OK");
     } else {
-     NSLog(@"dupa");   
+     NSLog(@"not ok");
     }
     CLLocationManager* locationManager = [[CLLocationManager alloc] init];
     locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
@@ -128,7 +147,7 @@ static NSString *const kClientID = @"829224129199-o2i8n6lijmj02sqftomb2g0h5gtan4
     // Dispose of any resources that can be recreated.
 }
 
-- (void)showPlacesForToday {
+- (void)querySheet {
     NSString *baseUrl = @"https://sheets.googleapis.com/v4/spreadsheets/";
     NSString *spreadsheetId = @"1jClAjuaKaIHeygHsYOxmAL4pQbZ0u6_hT7i6d1j7Mt0";
     NSString *range = @"September!A:F";
@@ -139,93 +158,163 @@ static NSString *const kClientID = @"829224129199-o2i8n6lijmj02sqftomb2g0h5gtan4
     [self.service fetchObjectWithURL:[NSURL URLWithString:baseUrl]
                          objectClass:[GTLObject class]
                             delegate:self
-                   didFinishSelector:@selector(displayPlacesForTodayWithServiceTicket:finishedWithObject:error:)];
+                   didFinishSelector:@selector(processQuerySheetResult:finishedWithObject:error:)];
 }
-- (IBAction)wayToWorkAction:(id)sender {
+
+- (void)openGoogleMapsWith:(NSString *) url {
     if ([[UIApplication sharedApplication] canOpenURL:
          [NSURL URLWithString:@"comgooglemaps://"]]) {
         [[UIApplication sharedApplication] openURL:
-         [NSURL URLWithString:@"comgooglemaps://?saddr=Faraona+11,+Pruszkow&daddr=Aleje+Jerozolimskie+92,+Warszawa&views=traffic&directionsmode=driving"]];
+         [NSURL URLWithString:url]];
     } else {
         NSLog(@"Can't use comgooglemaps://");
     }
+}
+
+- (IBAction)wayToWorkAction:(id)sender {
+    [self openGoogleMapsWith:kWorkURL];
 }
 - (IBAction)wayToHomeAction:(id)sender {
-    if ([[UIApplication sharedApplication] canOpenURL:
-         [NSURL URLWithString:@"comgooglemaps://"]]) {
-        [[UIApplication sharedApplication] openURL:
-         [NSURL URLWithString:@"comgooglemaps://?daddr=Faraona+11,+Pruszkow&saddr=Aleje+Jerozolimskie+92,+Warszawa&views=traffic&directionsmode=driving"]];
-    } else {
-        NSLog(@"Can't use comgooglemaps://");
-    }
+    [self openGoogleMapsWith:kHomeURL];
 }
+
 - (IBAction)analysisAction:(id)sender {
     if (!self.service.authorizer.canAuthorize) {
         // Not yet authorized, request authorization by pushing the login UI onto the UI stack.
         [self presentViewController:[self createAuthController] animated:YES completion:nil];
         
     } else {
-        [self showPlacesForToday];
+        [self querySheet];
     }
 }
 
-- (void)displayPlacesForTodayWithServiceTicket:(GTLServiceTicket *)ticket
+- (void)sendNotificationWith:(NSString *) msg withDelay:(NSTimeInterval)secs{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:secs];
+    notification.alertBody = msg;
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+- (void)setLeftMonit:(NSString *) text {
+    _textViewLeft.text = text;
+    _textViewLeft.textAlignment=NSTextAlignmentCenter;
+    _textViewLeft.font = [UIFont boldSystemFontOfSize:16];
+    _textViewLeft.textColor=[UIColor blackColor];
+}
+
+- (void)setRightMonit:(NSString *) text {
+    _textViewRight.text = text;
+    _textViewRight.textAlignment=NSTextAlignmentCenter;
+    _textViewRight.font = [UIFont boldSystemFontOfSize:16];
+    _textViewRight.textColor=[UIColor blackColor];
+}
+
+- (NSString*)getRowSelector:(NSDate *) date {
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"dd'.'MM"];
+    NSString *result = [df stringFromDate: date];
+    NSLog(@"result %@.", result);
+    return result;
+}
+
+- (NSString*)getShortRowSelector:(NSDate *) date {
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"dd"];
+    NSString *result = [df stringFromDate: date];
+    NSLog(@"result %@.", result);
+    return result;
+}
+
+- (NSArray*)findRowElements:(NSArray *) rows bySelector: (NSString*) selector {
+    NSMutableArray *result = [NSMutableArray arrayWithObjects:@"WOLNE",
+                              @"WOLNE", @"WOLNE", @"WOLNE", @"WOLNE", nil];
+    if (rows.count > 0) {
+        for (NSArray *row in rows) {
+            if(row.count > 1){
+                NSString *dateString = row[0];
+                if ([dateString containsString:selector]){
+                    for (int i=1; i<[row count]; i++) {
+                        NSString *value = row[i];
+                        if([value length]  != 0) {
+                            [result insertObject:row[i] atIndex:i-1];
+                            [result removeLastObject];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return [result copy];
+}
+
+- (NSString*) getReportMessage:(NSArray*) places {
+    NSMutableString *output = [[NSMutableString alloc] init];
+    [output appendFormat:@"[360->%@]\n[136->%@]\n[137->%@]\n[356->%@]\n[28->%@]",  places[0], places[1], places[2],  places[3], places[4]];
+    return [output copy];
+}
+
+- (NSArray*) getPlacesFrom:(NSArray*)rows  byDate:(NSDate*) date {
+    NSString *dateRowSelector = [self getRowSelector:date];
+    NSLog(@"dateRowSelector %@", dateRowSelector);
+    NSArray *places = [self findRowElements:rows bySelector:dateRowSelector];
+    return places;
+}
+
+-(int) countOcurrencesIn: (NSArray*) row byElementValue: (NSString*) elementValue{
+    int count = 0;
+    for (int i=0; i<[row count]; i++) {
+        NSString *value = row[i];
+        if([value length]  != 0 && [value containsString:elementValue]) {
+            count ++;
+        }
+    }
+    return count;
+}
+
+- (NSString*) getFastReportMessage:(NSArray*) rows byDate:(NSDate*) date {
+    NSMutableString *output = [[NSMutableString alloc] init];
+    NSString *dateRowSelector = [self getShortRowSelector:date];
+    NSArray *places = [self getPlacesFrom:rows byDate:date];
+    int freeCount = [self countOcurrencesIn:places byElementValue:@"WOLNE"];
+    int myCount = [self countOcurrencesIn:places byElementValue:@"MaciekP"];
+    NSLog(@"freeCount [%d]", freeCount);
+    NSLog(@"myCount [%d]", myCount);
+    NSString* reserved = myCount != 0 ? @"OK" : @"NO";
+    
+    [output appendFormat:@"[%@][FREE->%d][%@]\n",  dateRowSelector, freeCount, reserved];
+    return [output copy];
+}
+
+- (void)processQuerySheetResult:(GTLServiceTicket *)ticket
                     finishedWithObject:(GTLObject *)object
                                  error:(NSError *)error {
     NSLog(@"Result");
-    //NSDate *now = [NSDate date];
-    //NSDate *tommorow = [now dateByAddingTimeInterval:60*60*24*1];
-    //NSDate *dayAfterTommorow = [now dateByAddingTimeInterval:60*60*24*1];
-    
-    NSDateFormatter *df = [[NSDateFormatter alloc] init];
-    
-    [df setDateFormat:@"dd'.'MM"];
-    NSString *myDayString = [df stringFromDate:[NSDate date]];
-    
-    [df setDateFormat:@"mm"];
-    NSLog(@"myDayString %@.", myDayString);
-    if (error == nil) {
-        NSMutableString *output = [[NSMutableString alloc] init];
-        NSArray *rows = [object.JSON objectForKey:@"values"];
-        if (rows.count > 0) {
-            [output appendString:@""];
-            for (NSArray *row in rows) {
-                // Print columns A and E, which correspond to indices 0 and 4.
-                NSMutableArray *places = [NSMutableArray arrayWithObjects:@"WOLNE",
-                                    @"WOLNE", @"WOLNE", @"WOLNE", @"WOLNE", nil];
-                if(row.count > 1){
-                    NSString *dateString = row[0];
-                    if ([dateString containsString:myDayString]){
-                        
-                        for (int i=1; i<[row count]; i++) {
-                            NSString *value = row[i];
-                            if([value length]  != 0) {
-                               [places insertObject:row[i] atIndex:i-1];
-                            }
-                        }
-                        [output appendFormat:@"[360->%@]\n[136->%@]\n[137->%@]\n[356->%@]\n[28->%@]",  places[0], places[1], places[2],  places[3], places[4]];
-                        [output appendString:@"\n\n----------------------\n\n"];
-                        UILocalNotification *notification = [[UILocalNotification alloc] init];
-                        notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:1200];
-                        notification.alertBody = output;
-                        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
 
-                    }
-                    //[output appendFormat:@"%@ , %@, %@\n", row[0],  row[1], row[2]];
-                }
-            }
-        } else {
-            [output appendString:@"No data found."];
-        }
-        //self.output.text = output;
-        NSLog(@"OUT %@.", output);
-        _textView.layer.cornerRadius=5;
-        _textView.text = output;
-        _textView.textAlignment=NSTextAlignmentCenter;
-        _textView.font = [UIFont boldSystemFontOfSize:16];
-        _textView.textColor=[UIColor blackColor];
+    if (error == nil) {
+        NSArray *rows = [object.JSON objectForKey:@"values"];
+        NSDate *now = [NSDate date];
+        NSDate *now_plus_1 = [now dateByAddingTimeInterval:60*60*24*1];
+        NSDate *now_plus_2 = [now dateByAddingTimeInterval:60*60*24*2];
+        NSDate *now_plus_3 = [now dateByAddingTimeInterval:60*60*24*3];
+        NSDate *now_plus_4 = [now dateByAddingTimeInterval:60*60*24*4];
+        NSDate *now_plus_5 = [now dateByAddingTimeInterval:60*60*24*5];
         
+        NSArray *places = [self getPlacesFrom:rows byDate:now];
         
+        NSString *reportMsg = [self getReportMessage:places];
+        [self sendNotificationWith:reportMsg withDelay:1200];
+        [self setLeftMonit: reportMsg];
+        
+        NSMutableString *monit = [[NSMutableString alloc] init];
+        [monit appendString:[self getFastReportMessage:rows byDate:now_plus_1]];
+        [monit appendString:[self getFastReportMessage:rows byDate:now_plus_2]];
+        [monit appendString:[self getFastReportMessage:rows byDate:now_plus_3]];
+        [monit appendString:[self getFastReportMessage:rows byDate:now_plus_4]];
+        [monit appendString:[self getFastReportMessage:rows byDate:now_plus_5]];
+        [self setRightMonit: monit];
+        NSLog(@"OUT %@.", monit);
+        
+
     } else {
         NSMutableString *message = [[NSMutableString alloc] init];
         [message appendFormat:@"Error getting sheet data: %@\n", error.localizedDescription];
